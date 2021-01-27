9296a637b66d6cd063fce17439f2528e55551b40
JDJFisher <jordanfisher2096@gmail.com>
2021-01-27 17:14:43 +0000

    Started client request seeding

--- client/main.go
+++ client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"log"
 	"os"
 
@@ -19,6 +20,7 @@ func main() {
 		grpcHost = "master:6789"
 	}
 
+	// Open a connection to the master service
 	var conn *grpc.ClientConn
 	conn, err := grpc.Dial(grpcHost, grpc.WithInsecure())
 	if err != nil {
@@ -26,12 +28,35 @@ func main() {
 	}
 	defer conn.Close()
 
+	// Create storage client
 	storageClient := protos.NewStorageClient(conn)
+
+	// Load dummy requests from seeder
+	file, err := os.Open("seeder.csv")
+	if err != nil {
+		log.Fatal("Unable to read seeder file", err)
+	}
+	defer file.Close()
+	csvReader := csv.NewReader(file)
+	lines, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal("Unable to parse seeder as CSV", err)
+	}
+
+	// Loop over dummy requests
+	for i, line := range lines[1:] {
+
+		// TODO: Sleep for a few seconds
+
+		log.Println("Dispatching request", i, ":", line)
+
+		// TODO: Execute a Write if line has a value value otherwise do a read
+	}
 
 	response, err := storageClient.Read(context.Background(), &protos.ReadRequest{})
 	if err != nil {
 		log.Fatalf("Error reading from master - %v", err.Error())
+	} else {
+		log.Printf("Response from master: \n %s", response)
 	}
-
-	log.Printf("Read response from master: \n %s", response)
 }
